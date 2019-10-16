5340db8efba350c8e20deeda4729f55ddb48b2ea
lidonghui-github <1161656983@qq.com>
2019-10-16 14:20:44 +0800

    拦截器
    Signed-off-by: lidonghui-github <1161656983@qq.com>

---
+++ src/main/java/cn/hd/interceptor/FirstInterceptor.java
@@ -0,0 +1,42 @@
+package cn.hd.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FirstInterceptor implements HandlerInterceptor {
+    private String name;
+
+    public FirstInterceptor(String name) {
+        this.name = name;
+    }
+
+    public FirstInterceptor() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        System.out.println(name+"preHandle.........");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        System.out.println(name+"postHandle.........");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        System.out.println(name+"afterCompletion.........");
+    }
+}
